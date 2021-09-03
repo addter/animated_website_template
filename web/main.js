2253dcf4f17a84d19f6b9626c211a4dc3002a192
Sanjog Adhikari <40435154+addter@users.noreply.github.com>
2021-09-03 15:00:00 +0545

    web template
    This is an animated web template.

---
+++ web/main.js
@@ -0,0 +1,7 @@
+var blocks = document.getElementsByClassName('services-items');
+var container = document.getElementsByClassName('services');
+var hs = new HorizontalScroll.default({
+	blocks : blocks,
+	container: container,
+	spring: 1,
+});
